624154aa7a3bde14f17bb83e47420e4158ec941d
James Belk <78972994+jamesbelk0@users.noreply.github.com>
2022-06-30 18:07:52 -0400

    server schemas/server.js

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,65 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { Book, User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async(parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password');
+
+                return userData;
+            }
+
+            throw new AuthenticationError('No data');
+        }
+      },
+        Mutation: {
+            login: async (parent, { email, password }) => {
+                const user = await User.findOne({ email });
+          
+                if (!user) {
+                  throw new AuthenticationError('Incorrect credentials');
+                }
+          
+                const correctPw = await user.isCorrectPassword(password);
+          
+                if (!correctPw) {
+                  throw new AuthenticationError('Incorrect credentials');
+                }
+          
+                const token = signToken(user);
+                return { token, user };
+        },
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+                
+              const updatedUser = await User.findByIdAndUpdate(
+                { _id: context.user._id },
+                { $push: { savedBooks: bookData } },
+                { new: true }
+              );
+      
+              return updatedUser;
+            }
+      
+            throw new AuthenticationError('You need to select a book!');
+          },
+          removeBook: async (parent, args, context) => {
+            if (context.user) {
+                
+              const updatedUser = await User.findByIdAndDelete(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId} } },
+                { new: true }
+              );
+      
+              return updatedUser;
+            }
+      
+            throw new AuthenticationError('You need to select a book!');
+          },
+        }
+}
+module.exports = resolvers;
